a40beb8f0df1c16d986a2c0f0b67a728be8c604c
Stuzerain <62074382+Stuzerain@users.noreply.github.com>
2020-03-13 16:10:13 -0400

    Initial
    Contains arrays for states, abbreviations, and capitals, plus function that checks there are the same number of each.

---
+++ States/states.js
@@ -0,0 +1,35 @@
+var states = ["Alabama", "Alaska", "American Samoa", "Arizona", "Arkansas", "California",
+              "Colorado", "Connecticut", "Delaware", "District of Columbia", "Florida", 
+              "Georgia", "Guam", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", 
+              "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", 
+              "Minnesota", "Mississippi", "Missouri", "Montana", 
+              "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", 
+              "North Carolina", "North Dakota", "Northern Mariana Islands", "Ohio", "Oklahoma", 
+              "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", 
+              "South Dakota", "Tennessee", "Texas", "U.S. Virgin Islands", "Utah", "Vermont", 
+              "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
+
+var abbreviations = ["AK", "AL", "AR", "AS", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA", 
+                     "GU", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", 
+                     "MI", "MN", "MP", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", 
+                     "NV", "NY", "OH", "OK", "OR", "PA", "PR", "RI", "SC", "SD", "TN", "TX", 
+                     "UM", "UT", "VA", "VI", "VT", "WA", "WI", "WV", "WY"];
+
+var capitals = ["Montgomery", "Juneau", "Pago Pago", "Phoenix", "Little Rock", "Sacramento",
+                "Denver", "Hartford", "Dover", "Washington DC is the national capital",
+                "Tallahassee", "Atlanta", "Hagåtña", "Honolulu", "Boise", "springfield",
+                "Indianapolis", "Des Moines", "Topeka", "Frankfort", "Baton Rouge", "Augusta",
+                "Annapolis", "Boston", "Lansing", "St. Paul", "Jackson", "Jefferson City",
+                "Helena", "Lincoln", "Carson City", "Concord", "Trenton", "Santa Fe", "Albany",
+                "Raleigh", "Bismarck", "Saipan", "Columbus", "Oklahoma City", "Salem", "Harrisburg",
+                "San Juan", "Providence", "Columbia", "Pierre", "Nashville", "Austin",
+                "Charlotte Amalie", "Salt Lake City", "Montpelier", "Richmond", "Olympia", 
+                "Charleston", "Madison", "Cheyenne"];
+
+function numberCheck() {
+console.log(states.length);
+console.log(abbreviations.length);
+console.log(capitals.length);
+}
+
+numberCheck();
