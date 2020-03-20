77c2eafbf2597108c17282a32780d35f41ce067e
Stuzerain <62074382+Stuzerain@users.noreply.github.com>
2020-03-20 16:09:49 -0400

    Test
    Updated JavaScript for battleship, as old version was from a 1x6 test. This version does not process user inputs but demonstrates that assigning the classes hit and miss to cells properly shows the ship.png and miss.png images, respectively. This version also shows that displayMessage works as intended

--- Battleship/battleship.js
+++ Battleship/battleship.js
@@ -1,39 +1,24 @@
-//Declaring Variables
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
-var guess; 
-var hits = 0;
-var guesses = 0;
-var isSunk = false;
-
-//Creating loop and getting user guess
-while (isSunk == false) {
-	guess = prompt("Ready, aim, fire! (enter a number 0-6):");
-//Checking guesses for validity
-	if (guess < 0 || guess > 6) {
-		alert("Please entera a valid cell number!");
-	} else {
-		guesses = guesses + 1;
-
-//Checking guesses for hits
-if (guess == location1 || guess == location2 || guess == location3) {
-	alert("HIT!");
-	hits = hits + 1;
-	
-	
-
-//Checking for sinkage and alerting user
-if (hits == 3) {
-	isSunk = true;
-	alert("You sank my battleship!");
-			}
-		}else {
-			alert("MISS!");
-		}
+var view = {
+	displayMessage: function(msg) {
+		var messageArea = document.getElementById("messageArea");
+		messageArea.innerHTML = msg;
+	},
+	displayHit: function(location) {
+		var cell = document.getElementById(location);
+		cell.setAttribute("class", "hit");
+	},
+	displayMiss: function(location) {
+		var cell = document.getElementById(location);
+		cell.setAttribute("class", "miss");
 	}
-}
-	
-//Providing stats to user post-game
-var stats = "You took " + guesses + " guesses to sink the battleship, " + "which means your accuracy was " + (3/guesses);
-alert(stats);
+};
+
+//Testing hits and misses on grid
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
+
+view.displayMessage("We out here testing and s**t");
