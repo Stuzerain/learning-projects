0425da00f106ae839c2820d72db8f881996078fe
Stuzerain <62074382+Stuzerain@users.noreply.github.com>
2020-03-25 16:40:10 -0400

    Functional but simple
    This version includes a functional quiz with three multiple-choice questions and styling. The questions appear in random order and buttons are used for navigating the quiz. Currently, the possible answers to the questions are hardcoded. Things to think about for the future: 
    -more questions
    -randomized answers (so options other than answer are not hardcoded)
    -status summary at end of quiz ("You got x/y correct.")

---
+++ National Capitals Quiz/quiz.js
@@ -0,0 +1,110 @@
+const startButton = document.getElementById("start-btn")
+const nextButton = document.getElementById("next-btn")
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question")
+const answerButtonsElement = document.getElementById("answer-btns")
+
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener("click", startGame)
+nextButton.addEventListener("click", () => {
+    (currentQuestionIndex = currentQuestionIndex + 1)
+    setNextQuestion()
+})
+
+
+function startGame() {
+    startButton.classList.add("hide")
+    shuffledQuestions = questions.sort(() => Math.random() -.5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove("hide")
+    setNextQuestion()
+}
+
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add("hide")
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
+    }
+}
+
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove("hide")
+    } else {
+        startButton.innerText = "Restart"
+        startButton.classList.remove("hide")
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add("correct")
+    } else {
+        element.classList.add("wrong")
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove("correct")
+    element.classList.remove("wrong")
+}
+
+const questions = [
+    {
+        question: "What is the capital of the United Arab Emirates?",
+        answers:[
+            { text: "Abu Dhabi", correct: true },
+            { text: "Amman", correct: false },
+            { text: "Baghdad", correct: false },
+            { text: "Doha", correct: false }
+        ]
+    }, 
+    {
+        question: "What is the capital of Ireland?",
+        answers:[
+            { text: "Dublin", correct: true },
+            { text: "Belfast", correct: false },
+            { text: "Douglas", correct: false },
+            { text: "London", correct: false }
+        ]
+    }, 
+    {
+        question: "What is the capital of South Korea?",
+        answers:[
+            { text: "Seoul", correct: true },
+            { text: "Pyongyang", correct: false },
+            { text: "Phnom Penh", correct: false },
+            { text: "Taipei", correct: false }
+        ]
+    }, 
+]
