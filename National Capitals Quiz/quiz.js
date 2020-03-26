29f634d2d699a75bb49660c37fc468be6f38ec61
Stuzerain <62074382+Stuzerain@users.noreply.github.com>
2020-03-26 15:18:52 -0400

    Answer randomization working but not well
    The randomization seems to only be able to select from several items in each array, and on one pass through I had "Seoul" four times--once for correct answer and three times as a 'random' wrong answer. Needs work.

--- National Capitals Quiz/quiz.js
+++ National Capitals Quiz/quiz.js
@@ -7,6 +7,85 @@ const answerButtonsElement = document.getElementById("answer-btns")
 
 let shuffledQuestions, currentQuestionIndex
 
+const continents = {
+    asia: [
+        "Kabul", "Yerevan", "Baku", "Manama", "Dhaka", "Thimphu", "Bandar Seri Begawan", 
+        "Phnom Penh", "Beijing", "Nicosia", "Tbilisi", "New Delhi", "Jakarta", "Tehran", 
+        "Baghdad", "Tokyo", "Amman", "Nur-Sultan", "Kuwait City", "Bishkek", "Vientiane",
+        "Beirut", "Kuala Lumpur", "Malé", "Ulaanbaatar", "Naypyidaw", "Kathmandu", "Pyongyang",
+        "Muscat", "Islamabad", "Manila", "Doha", "Moscow", "Riyadh", "Singapore", "Seoul", 
+        "Sri Jayawardenepura Kotte", "Damascus", "Taipei", "Dushanbe", "Bangkok", "Dili",
+        "Ankara", "Ashgabat", "Abu Dhabi", "Tashkent", "Hanoi", "Sana'a"
+    ],
+
+    europe: [
+        "Tirana", "Andorra la Vella", "Yerevan", "Vienna", "Baku", "Minsk", "Brussels", 
+        "Sarajevo", "Sofia", "Zagreb", "Nicosia", "Prague", "Copenhagen", "Tallinn", 
+        "Helsinki", "Paris", "Tbilisi", "Berlin", "Athens", "Budapest", "Reykjavik", 
+        "Dublin", "Rome", "Nur-Sultan", "Pristina", "Riga", "Vaduz", "Vilnius", 
+        "Luxembourg", "Valetta", "Chisinau", "Monaco", "Podgorica", "Amsterdam", "Skopje", 
+        "Oslo", "Warsaw", "Lisbon", "Bucharest", "Moscow", "San Marino", "Belgrade",
+        "Bratislava", "Ljubljana", "Madrid", "Stockholm", "Bern", "Ankara", "Kiev",
+        "London", "Vatican City"
+    ],
+
+    africa: [
+        "Algiers", "Luanda", "Porto-Novo", "Gaborone", "Ouagadougou", "Gitega", "Praia",
+        "Yaounde", "Bangui", "N'Djamena", "Moroni", "Kinshasa", "Brazzaville", "Yamooussoukro",
+        "Djibouti", "Cairo", "Malabo", "Asmara", "Mbabane (administrative) and Lobamba (legislative/royal)",
+        "Addis Ababa", "Libreville", "Banjul", "Accra", "Conakry", "Bissau", "Nairobi", "Maseru", 
+        "Monrovia", "Tripoli", "Antananarivo", "Lilongwe", "Bamako", "Nouakchott", "Port Louis",
+        "Rabat", "Maputo", "Windhoek", "Niamey", "Abuja", "Kigali", "São Tomé", "Dakar",
+        "Victoria", "Freetown", "Mogadishu", "Juba", "Khartoum", "Dodoma", "Lomé", "Tunis",
+        "Pretoria (administrative), Cape Town(legistlative), Bloemfontein (Judicial)",
+        "Kampala", "Lusaka", "Harare"
+    ],
+
+    americas: [
+        "Saint John's", "Nassau", "Bridgetown", "Belmopan", "Ottawa", "San Jose", "Havana",
+        "Roseau", "Santo Domingo", "San Salvador", "Saint George's", "Guatemala City", 
+        "Port-au-Prince", "Tegucigalpa", "Kingston", "Mexico City", "Managua", "Panama City",
+        "Basseterre", "Castries", "Kingstown", "Port of Spain", "Washington, D.C.", 
+        "Buenos Aires", "Sucre (de jure), La Paz (seat of government", "Brasilia", "Santiago",
+        "Bogotá", "Quito", "Georgetown", "Asunción", "Lima", "Paramaribo", "Montevideo", "Caracas"
+    ]
+}
+
+let wrongAnswerAsia = continents.asia[Math.floor(Math.random() * continents.asia.length)]
+let wrongAnswerEurope = continents.europe[Math.floor(Math.random() * continents.europe.length)]
+let wrongAnswerAfrica = continents.africa[Math.floor(Math.random() * continents.africa.length)]
+let wrongAnswerAmericas = continents.americas[Math.floor(Math.random() * continents.americas.length)]
+
+const questions = [
+    {
+        question: "What is the capital of the United Arab Emirates?",
+        answers:[
+            { text: "Abu Dhabi", correct: true },
+            { text: wrongAnswerAsia, correct: false },
+            { text: wrongAnswerAsia, correct: false },
+            { text: wrongAnswerAfrica, correct: false }
+        ]
+    }, 
+    {
+        question: "What is the capital of Ireland?",
+        answers:[
+            { text: "Dublin", correct: true },
+            { text: wrongAnswerEurope, correct: false },
+            { text: wrongAnswerEurope, correct: false },
+            { text: wrongAnswerEurope, correct: false }
+        ]
+    }, 
+    {
+        question: "What is the capital of South Korea?",
+        answers:[
+            { text: "Seoul", correct: true },
+            { text: wrongAnswerAsia, correct: false },
+            { text: wrongAnswerAsia, correct: false },
+            { text: wrongAnswerAsia, correct: false }
+        ]
+    }, 
+]
+
 startButton.addEventListener("click", startGame)
 nextButton.addEventListener("click", () => {
     (currentQuestionIndex = currentQuestionIndex + 1)
@@ -79,76 +158,3 @@ function clearStatusClass(element) {
     element.classList.remove("wrong")
 }
 
-const questions = [
-    {
-        question: "What is the capital of the United Arab Emirates?",
-        answers:[
-            { text: "Abu Dhabi", correct: true },
-            { text: "Amman", correct: false },
-            { text: "Baghdad", correct: false },
-            { text: "Doha", correct: false }
-        ]
-    }, 
-    {
-        question: "What is the capital of Ireland?",
-        answers:[
-            { text: "Dublin", correct: true },
-            { text: "Belfast", correct: false },
-            { text: "Douglas", correct: false },
-            { text: "London", correct: false }
-        ]
-    }, 
-    {
-        question: "What is the capital of South Korea?",
-        answers:[
-            { text: "Seoul", correct: true },
-            { text: "Pyongyang", correct: false },
-            { text: "Phnom Penh", correct: false },
-            { text: "Taipei", correct: false }
-        ]
-    }, 
-]
-
-const continents = [
-    asia = [
-        "Kabul", "Yerevan", "Baku", "Manama", "Dhaka", "Thimphu", "Bandar Seri Begawan", 
-        "Phnom Penh", "Beijing", "Nicosia", "Tbilisi", "New Delhi", "Jakarta", "Tehran", 
-        "Baghdad", "Tokyo", "Amman", "Nur-Sultan", "Kuwait City", "Bishkek", "Vientiane",
-        "Beirut", "Kuala Lumpur", "Malé", "Ulaanbaatar", "Naypyidaw", "Kathmandu", "Pyongyang",
-        "Muscat", "Islamabad", "Manila", "Doha", "Moscow", "Riyadh", "Singapore", "Seoul", 
-        "Sri Jayawardenepura Kotte", "Damascus", "Taipei", "Dushanbe", "Bangkok", "Dili",
-        "Ankara", "Ashgabat", "Abu Dhabi", "Tashkent", "Hanoi", "Sana'a"
-    ],
-
-    europe = [
-        "Tirana", "Andorra la Vella", "Yerevan", "Vienna", "Baku", "Minsk", "Brussels", 
-        "Sarajevo", "Sofia", "Zagreb", "Nicosia", "Prague", "Copenhagen", "Tallinn", 
-        "Helsinki", "Paris", "Tbilisi", "Berlin", "Athens", "Budapest", "Reykjavik", 
-        "Dublin", "Rome", "Nur-Sultan", "Pristina", "Riga", "Vaduz", "Vilnius", 
-        "Luxembourg", "Valetta", "Chisinau", "Monaco", "Podgorica", "Amsterdam", "Skopje", 
-        "Oslo", "Warsaw", "Lisbon", "Bucharest", "Moscow", "San Marino", "Belgrade",
-        "Bratislava", "Ljubljana", "Madrid", "Stockholm", "Bern", "Ankara", "Kiev",
-        "London", "Vatican City"
-    ],
-
-    africa = [
-        "Algiers", "Luanda", "Porto-Novo", "Gaborone", "Ouagadougou", "Gitega", "Praia",
-        "Yaounde", "Bangui", "N'Djamena", "Moroni", "Kinshasa", "Brazzaville", "Yamooussoukro",
-        "Djibouti", "Cairo", "Malabo", "Asmara", "Mbabane (administrative) and Lobamba (legistlative/royal)",
-        "Addis Ababa", "Libreville", "Banjul", "Accra", "Conakry", "Bissau", "Nairobi", "Maseru", 
-        "Monrovia", "Tripoli", "Antananarivo", "Lilongwe", "Bamako", "Nouakchott", "Port Louis",
-        "Rabat", "Maputo", "Windhoek", "Niamey", "Abuja", "Kigali", "São Tomé", "Dakar",
-        "Victoria", "Freetown", "Mogadishu", "Juba", "Khartoum", "Dodoma", "Lomé", "Tunis",
-        "Pretoria (administrative), Cape Town(legistlative), Bloemfontein (Judicial)",
-        "Kampala", "Lusaka", "Harare"
-    ],
-
-    americas = [
-        "Saint John's", "Nassau", "Bridgetown", "Belmopan", "Ottawa", "San Jose", "Havana",
-        "Roseau", "Santo Domingo", "San Salvador", "Saint George's", "Guatemala City", 
-        "Port-au-Prince", "Tegucigalpa", "Kingston", "Mexico City", "Managua", "Panama City",
-        "Basseterre", "Castries", "Kingstown", "Port of Spain", "Washington, D.C.", 
-        "Buenos Aires", "Sucre (de jure), La Paz (seat of government", "Brasilia", "Santiago",
-        "Bogotá", "Quito", "Georgetown", "Asunción", "Lima", "Paramaribo", "Montevideo", "Caracas"
-    ]
-]
