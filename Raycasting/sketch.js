2578fa4692d2a313626a32de57cecd4f9169efbb
Stuzerain <62074382+Stuzerain@users.noreply.github.com>
2020-05-13 16:24:16 -0400

    Toggle random particle movement, notes
    Added toggle for random movement of particle with global boolean 'randomMove'. Also changed toggle for random boundary generation to 'randomWalls' to prevent confusion between these two variables. slightly edited drawing of walls around edge of canvas because the corners left a slight gap where rays did not detect a boundary. Added notes to blocks of code for legibility.

--- Raycasting/sketch.js
+++ Raycasting/sketch.js
@@ -1,17 +1,16 @@
-
-
-
 let walls = [];
 let ray;
 let particle;
 let xoff = 0;
 let yoff = 10000;
-let randoms = false;
+let randomWalls = false;
+let randomMove = false;
 
 function setup() {
     createCanvas(1000, 1000);
 
-    if (randoms) {
+    //5 random intial walls--off by default but toggle is global randomWalls boolen
+    if (randomWalls) {
     for (let i = 0; i < 5; i++) {
         let x1 = random(width);
         let x2 = random(width);
@@ -22,10 +21,12 @@ function setup() {
 }
     
     //walls around edges of canvas
-    walls.push(new Boundary(0, 0, width, 0));
-    walls.push(new Boundary(width, 0, width, height));
-    walls.push(new Boundary(width, height, 0, height));
-    walls.push(new Boundary(0, height, 0, 0));
+    // +/- 5 values used because using only 0, width, and height did not create a recognizable boundary
+    //at corners when particle was sitting at (width / 2, height / 2) and left a visible gap in rays
+    walls.push(new Boundary(-5, 0, width + 5, 0));
+    walls.push(new Boundary(width, 0, width, height + 5));
+    walls.push(new Boundary(width + 5, height + 5, -5, height));
+    walls.push(new Boundary(-5, height + 5, -5, 0));
 
     particle = new Particle();
 }
@@ -37,26 +38,35 @@ function draw() {
     for (let wall of walls) {
         wall.show();
     }
-    particle.update(noise(xoff) * width, noise(yoff) * height);
+
     particle.show();
     particle.look(walls);
 
+    //random movement or not--toggle is global randomMove boolean
+    if (randomMove) {
+        particle.update(noise(xoff) * width, noise(yoff) * height);
+    
+    }
+
+    if (!randomMove) {
+        particle.update(width / 2, height / 2);
+    }
+
+    //random movement of particle; 0.005 moves slowly around screen and is a good initial value
+    //lower values mean slower movement
     xoff += 0.005;
     yoff += 0.005;
 
+    //drawing new boundaries by dragging mouse
     stroke(255);
     if (mouseIsPressed === true) {
       walls.push(new Boundary(mouseX, mouseY, pmouseX, pmouseY));
     }
 
-    //testing to see 'bubble' where ray collides w/ boundary; outdated
-    //let pt = ray.cast(wall);
+    //testing to see 'bubble' where ray collides w/ boundary
+    //let pt = ray.cast(walls);
     //if (pt) {
     //    fill(255);
-    //    ellipse(pt.x, pt.y, 8, 8)
+    //    ellipse(wall.x, wall.y, 8, 8)
     //}
 }
-
-
-
-
